b8078ccdb46f36ef5a54821dd35a232d19ade637
anshaoyuan <anshaoyuan@gmail.com>
2013-07-01 12:10:01 +0800

    refactor find element methods

--- business/common/common_module.rb
+++ business/common/common_module.rb
@@ -1,49 +1,49 @@
 #encoding:utf-8
 module Common
 	def jump_to_my_space
-		a_link =@wait.until{ @driver.find_element(:id,"right_userName") }
+		a_link = get_element_by_id("right_userName")
 		a_link.click
 	end
 	def show_my_follow_user
 		jump_to_my_space
-		my_follow_btn = @wait.until{@driver.find_element(:id,"myattention-lxj")}
+		my_follow_btn = get_element_by_id("myattention-lxj")
 		my_follow_btn.click
 		wait(3)
 	end
 	def show_my_new_fans_user
 		jump_to_my_space
-		my_remind_btn = @wait.until{@driver.find_element(:id,"myremind-lxj")}
+		my_remind_btn = get_element_by_id("myremind-lxj")
 		my_remind_btn.click
-		new_fans_li = @wait.until{@driver.find_element(:id,"fans-l")}
+		new_fans_li = get_element_by_id("fans-l")
 		new_fans_li.find_element(:tag_name,"a").click
 		wait(3)
 	end
 	def show_my_fans_user
 		jump_to_my_space
-		my_fans_btn = @wait.until{@driver.find_element(:id,"myfans-lxj")}
+		my_fans_btn = get_element_by_id("myfans-lxj") 
 		my_fans_btn.click
 		wait(3)
 	end
 	def show_my_chat_history
 		jump_to_my_space
-		my_talk = @wait.until{@driver.find_element(:id,"mynotes-lxj")}
+		my_talk = get_element_by_id("mynotes-lxj")
 		my_talk.click
 		wait(3)
 	end
 	def show_my_store_stream
 		jump_to_my_space
-		my_store = @wait.until{@driver.find_element(:id,"mystore-lxj")}
+		my_store = get_element_by_id("mystore-lxj")
 		my_store.click
 		wait(3)
 	end
 	def get_option_from_component_by_name(component_id,name)
-		component = @wait.until{@driver.find_element(:id,component_id)}
-		show_team_a = component.find_element(:tag_name,"a")
+		component = get_element_by_id(component_id)
+		show_team_a = get_element_by_tag_name("a",component)
 		show_team_a.click
-		input=@wait.until{ component.find_element(:css,"div div input")}
+		input= get_element_by_css("div div input",component) 
 		input.send_keys name
 		wait(1)
-		lis = component.find_elements(:tag_name,"li")
+		lis = get_elements_by_tag_name("li",component)
 		select_option_from_lis_by_search_name(lis,name)
 	end
 	def select_option_from_lis_by_search_name(lis,search_name)
@@ -62,14 +62,14 @@ module Common
 	end
 	def set_option_to_input(member_array,div_id)
 		return if member_array.nil?
-		input = @wait.until{@driver.find_element(:css,"##{div_id} ul li input.default")}
+		input = get_element_by_css("##{div_id} ul li input.default")
 		input.clear
 		select_option_by_members(member_array,input,div_id)
 		
 	end
 	def set_option_to_input_by_class(member_array,div_id)
 		return if member_array.nil?
-		input = @wait.until{@driver.find_element(:css,"##{div_id} div div ul li.search-field input")}
+		input = get_element_by_css("##{div_id} div div ul li.search-field input") 
 		select_option_by_members(member_array,input,div_id)
 	end
 	def select_option_by_members(member_array,input,div_id)
@@ -77,7 +77,7 @@ module Common
 			input.send_keys member
 			wait(1)
 			input.click
-			members = @wait.until{@driver.find_elements(:css,"##{div_id} div ul li")}
+			members = get_elements_by_css("##{div_id} div ul li")
 			members.each do |li|
 				if li.text.include?member
 					li.click
@@ -88,7 +88,7 @@ module Common
 		end
 	end
 	def get_li_from_option_by_name(div_id,name)
-		options = @wait.until{@driver.find_elements(:css,"##{div_id} div.accordion-inner div ul li")}
+		options = get_elements_by_css("##{div_id} div.accordion-inner div ul li")
 		options.each do |li|
 			return li if li.text.include?name
 		end
@@ -97,14 +97,14 @@ module Common
 	def set_option_to_input_by_search(member_array,div_id)
 		return if member_array.nil?
 			begin
-				tag_input = @wait.until{@driver.find_element(:css,"##{div_id} ul li input.default")}
+				tag_input = get_element_by_css("##{div_id} ul li input.default")
 				tag_input.clear
 				member_array.each do |tag|
 					tag_input.send_keys tag
-					search_btn = @wait.until{@driver.find_element(:css,"##{div_id} ul li a")}
+					search_btn = get_element_by_css("##{div_id} ul li a")
 					search_btn.click
 					wait(3)
-					tag_list = @wait.until{@driver.find_elements(:css,"##{div_id} div ul li")}
+					tag_list = get_elements_by_css("##{div_id} div ul li")
 					if tag_list.length>0
 						tag_list[0].click
 						@log.info("#{tag} add successfully")
@@ -118,32 +118,56 @@ module Common
 			end
 	end
 	def change_login(login_name,pwd)
-        @wait.until{@driver.find_element(:id,"username")}.click
-        @wait.until{@driver.find_element(:id,"changeUserId")}.click
-        username = @wait.until{@driver.find_element(:id,"signin-email")}
+        get_element_by_id("username").click
+        get_element_by_id("changeUserId").click
+        username = get_element_by_id("signin-email")
         username.clear
         username.send_keys login_name
-        userpwd = @wait.until{@driver.find_element(:id,"signin-password")}
+        userpwd = get_element_by_id("signin-password")
         userpwd.clear
         userpwd.send_keys pwd
-        @driver.find_element(:id,"loginBtn").click
+        get_element_by_id("loginBtn").click
         wait(5)
-    	change_panel_class = @driver.find_element(:id,"switcher").attribute("class")
+    	change_panel_class = get_element_by_id("switcher").attribute("class")
     	!change_panel_class.include?("in")
     end
     def update_info_by_typename(name,info)
-		typename = @wait.until{@driver.find_element(:name,name)}
+		typename = get_element_by_name(name)
 		typename.clear
 		typename.send_keys info
 	end
 	def is_update?(name,info)
 		wait(3)
-		@wait.until{@driver.find_element(:name,name)}.attribute("value").eql?info
+		get_element_by_name(name).attribute("value").eql?info
 	end
-	def get_element_by_name(name)
-		@wait.until{@driver.find_element(:name,name)}
+	def get_element_by_name(name,finder = @driver)
+		@wait.until{finder.find_element(:name,name)}
 	end
-	def get_element_by_id(id)
-		@wait.until{@driver.find_element(:id,id)}
+	def get_element_by_id(id,finder = @driver)
+		@wait.until{finder.find_element(:id,id)}
+	end
+	def get_element_by_css(css,finder = @driver)
+		@wait.until{finder.find_element(:css,css)}
+	end
+	def get_elements_by_css(css,finder = @driver)
+		@wait.until{finder.find_elements(:css,css)}
+	end
+	def get_element_by_tag_name(tag_name,finder = @driver)
+		@wait.until{finder.find_element(:tag_name,tag_name)}
+	end
+	def get_elements_by_tag_name(tag_name,finder = @driver)
+		@wait.until{finder.find_elements(:tag_name,tag_name)}
+	end
+	def get_element_by_link_text(link_text,finder = @driver)
+		@wait.until{finder.find_element(:link_text,link_text)}
+	end
+	def get_elements_by_link_text(link_text,finder = @driver)
+		@wait.until{finder.find_elements(:link_text,link_text)}
+	end
+	def get_element_by_class_name(class_name,finder = @driver)
+		@wait.until{finder.find_elements(:class_name,class_name)}
+	end
+	def get_elements_by_class_name(class_name,finder = @driver)
+		@wait.until{find.find_elements(:class_name,class_name)}
 	end
 end
