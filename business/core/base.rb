13219c7bc55cdd56de30c73db1d02318315681f5
anshaoyuan <anshaoyuan@gmail.com>
2013-07-01 11:06:44 +0800

    refactor base and help code

--- business/core/base.rb
+++ business/core/base.rb
@@ -8,31 +8,27 @@ require 'time'
 require_relative 'help_'
 class Base
 
-	
 	def initialize(login_info = Config_Option::LOGIN_INFO)
 		@login_info = login_info
 		@driver=LoginHelp.login_method(login_info[:login_name],login_info[:pwd])
 		@log=LogHelp.getLog
 		@wait = get_wait(20)
 		@title="title"+Time.now.strftime("%Y-%m-%d")
-		if(@driver.nil? == false)
-			@log.setDriver(@driver)
-		end
-		
+		@log.setDriver(@driver) unless @driver.nil?
 	end
 	
 	def closeDriver
 		begin
-			@driver.close if !@driver.nil?
+			@driver.close unless @driver.nil?
 			true
 		rescue Exception => e
 			false
 		end
 	end
+
 	def wait(second)
 		begin
-			error_wait = get_wait(second)
-			error_wait.until{@driver.find_element(:id,"cannotfind")}
+			get_wait(second).until{@driver.find_element(:id,"cannotfind")}
 		rescue
 			#this error needn't to catch
 		end
@@ -48,9 +44,11 @@ class Base
 	def getDriver
 		@driver
 	end
+
 	def getLog
 		@log
 	end
+
 	def goto_main_page
 		@driver.get Config_Option::LOGIN_URL
 	end
--- business/core/help_.rb
+++ business/core/help_.rb
@@ -2,46 +2,36 @@
 require 'logger'
 require 'time'
 
-
-
 class Logger
   #这个变量设置info日志级别时是否截图
   #include PictureHelp
   INFO_P=true
-
   ERROR_P=true
   attr :driver
   alias :old_info :info
-
   alias :old_error :error
 
   def setDriver(driver)
-    puts "driver here"
     @driver = driver
   end
-  def info(*args, &block)
 
+  def info(*args, &block)
     old_info(args[0],&block)
     if(INFO_P)
       p_path = save_p()
-       old_info("see the picture#{p_path}")
+      old_info("see the picture#{p_path}")
     end
-    
   end
+
   def error(*args,&block)
-
     old_error(args[0],&block)
-
     if(ERROR_P)
       p_path = save_p()
       old_error("see the picture#{p_path}")
     end
   end
-  def save_p()
 
-    if @driver.nil?
-      return
-    end
+  def save_p()
     path = File.expand_path(File.dirname(__FILE__))
     time = Time.now.strftime("%Y-%m-%d-%H-%M-%S")
     date = Time.now.strftime("%Y-%m-%d")
@@ -51,27 +41,28 @@ class Logger
     pictureDir = path+"/../../picture/"+date
     Dir.mkdir pictureDir if !File.directory?(pictureDir)
     @p_name = pictureDir+'/'+time+'_p.png'
-    begin
-       @driver.save_screenshot @p_name
-    rescue Exception => e
-      puts @driver.class
-      puts "save_screenshot fail"
-    end
-   
+    take_screenshot(@p_name)
     File.expand_path(pictureDir)+'/'+time+'_p.png'
   end
+
+  private
+  def take_screenshot(name)
+    begin
+       @driver.save_screenshot name
+    rescue Exception => e
+      old_error("#{@p_name}截图失败 ==> #{e.message}")
+    end
+  end
+
 end
+
 class LogHelp
   def self.getLog
-
     path = File.expand_path(File.dirname(__FILE__))
     time = Time.now.strftime("%Y-%m-%d")
-    logsDir =path+"/../../logs"
-
-    Dir.mkdir logsDir  if !File.directory?(logsDir)
-
+    logsDir = path+"/../../logs"
+    Dir.mkdir logsDir  unless File.directory?(logsDir)
     log_file_name = logsDir+"/"+time+'.log'
     Logger.new(log_file_name, 'daily')
-    
   end
 end
